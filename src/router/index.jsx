22b84cf14a6ee40b451b718d42bca1282e5dab86
AChristoff <aleksov101@gmail.com>
2020-07-22 20:49:06 +0300

    refactor routing component

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,20 @@
 import React, {lazy, Suspense} from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import NotFound from "../components/pages/NotFound";
-import Home from "../components/pages/home/Home";
+const Home = lazy(() => import('../components/pages/home/Home'));
+const Login = lazy(() => import('../components/pages/user/Login'));
+const Register = lazy(() => import('../components/pages/user/Register'));
+const RegisterConfirm = lazy(() => import('../components/pages/user/RegisterConfirm'));
+const NotFound = lazy(() => import('../components/pages/NotFound'));
 
 export function RouterMain() {
   return (
-    <Suspense fallback={() => <p>Loading...</p>}>
+    <Suspense fallback={<span>Loading...</span>}>
       <Switch>
         <Route path='/' exact component={Home}/>
-
-        <Route path='/user/login' exact component={lazy(() => import('../components/pages/user/Login'))}/>
-        <Route path='/user/register' exact component={lazy(() => import('../components/pages/user/Register'))}/>
-        <Route path='/user/register/confirm' exact component={lazy(() => import('../components/pages/user/RegisterConfirm'))}/>
-
+        <Route path='/user/login' exact component={Login}/>
+        <Route path='/user/register' exact component={Register}/>
+        <Route path='/user/register/confirm' exact component={RegisterConfirm}/>
         <Route component={NotFound}/>
       </Switch>
     </Suspense>
