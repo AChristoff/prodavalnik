f6d4bda951ec9aaa9451c54842106fd72193d847
AChristoff <aleksov101@gmail.com>
2020-07-26 19:32:52 +0300

    pagination

--- src/components/shared/Pagination.jsx
+++ src/components/shared/Pagination.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Pagination from '@material-ui/lab/Pagination';
+import {Redirect} from "react-router-dom";
 
 
 class OffersPagination extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: '1',
+      nexPage: '1'
+    };
+  }
 
   handleChange = (event, value) => {
-    console.log(value);
+    this.setState({
+      nexPage: value.toString(),
+    })
   };
 
-
   render() {
+
+    if(this.state.currentPage !== this.state.nexPage){
+      return <Redirect to={`/offers/all/${this.state.nexPage}/4/createdAt/-1`} />
+    }
+
     return (
       <div>
         <Pagination
