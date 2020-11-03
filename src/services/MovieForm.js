749f4ebe9d1860967f40378acaf9e98aa22c07e5
aleksov <aleksov101@gmail.com>
2020-11-03 22:51:16 +0200

    add tests

--- src/services/MovieForm.js
+++ src/services/MovieForm.js
@@ -15,10 +15,12 @@ export default class MovieForm extends Component {
         data-testid="movie-form" 
         onSubmit={
           () => submitForm({
-            text
+            text,
           })
       }>
-        <input type="text"/>
+    
+        <label htmlFor="text">Label</label>
+        <input type="text" id="text" onChange={e => this.setState({text: e.target.value})}></input>
         <button type="submit">Submit</button>
       </form>
     )
--- src/services/MovieForm.test.js
+++ src/services/MovieForm.test.js
@@ -7,12 +7,20 @@ afterEach(cleanup);
 const onSubmit = jest.fn();
 
 test('<MovieForm>', () => {
-  const { debug, getByTestId, queryByTestId, container, getByText } = render(<MovieForm submitForm={onSubmit}/>);
+  const { debug, getByTestId, queryByTestId, container, getByText, getByLabelText } = render(<MovieForm submitForm={onSubmit}/>);
 
   expect(queryByTestId('movie-form')).toBeTruthy();
 
+  // getByLabelText('Label').value = 'hello';
+  // fireEvent.change(getByLabelText('Label'));
+
+  fireEvent.change(getByLabelText('Label'), { 
+    target: { value: 'hello'}
+  });
+
   fireEvent.click(getByText('Submit'));
 
-  expect(onSubmit).toHaveBeenCalledTimes(1);
+  expect(onSubmit).toHaveBeenCalledTimes(1); 
+  expect(onSubmit).toHaveBeenCalledWith({text: "hello"});
   // debug();
 })
