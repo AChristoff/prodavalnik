e1f61a1da1b0d7a06859ef5b4f05dce7be758c60
aleksov <aleksov101@gmail.com>
2020-11-08 09:30:54 +0200

    add token exp check

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Footer from "./components/shared/main/Footer";
 import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
 import Main from "./components/shared/main/Main";
 import {ThemeContext} from "./context/theme-context";
+import {AuthContext} from "./context/user-context";
 import SnackbarAlert from "./components/shared/snackbar/Snackbar";
+import jwt_decode from "jwt-decode";
 
 export default function App() {
   const {isLightTheme} = useContext(ThemeContext);
+  const {updateUserContext} = useContext(AuthContext);
   const [lightTheme, setLightTheme] = useState(true);
 
   const theme = createMuiTheme({
@@ -22,10 +25,23 @@ export default function App() {
     },
   });
 
+   //Component did mount
+   useEffect(() => {
+     isTokenExpired();
+   },[])
+
   //Component did update
   useEffect(() => {
     setLightTheme(isLightTheme)
   }, [lightTheme]);
+
+  const isTokenExpired = () => {
+    const token = window.localStorage.getItem('token');
+    const decodedToken = jwt_decode(token);
+    const isTokenExpired = decodedToken.exp * 1000 < Date.now();
+    
+    updateUserContext('isAuth', !isTokenExpired)
+  };
 
   return (
     <div className={isLightTheme ? 'app light' : 'app dark'}>
